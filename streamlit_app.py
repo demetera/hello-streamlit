50b9df2cdfe5afac43f45e1a04d82e20f5365705
Demeter Abroncs <hal4ed@gmail.com>
2025-03-03 17:14:47 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,8 +3,8 @@ import pandas as pd
 
 df = pd.DataFrame(
     [
-       {"item": "What", "index": 4, "bought": True},
-       {"item": "Propagate", "index": 3, "bought": True},
+       {"item": "Item 1", "index": 4, "bought": True},
+       {"item": "Item 2", "index": 3, "bought": True},
    ]
 )
 edited_df = st.data_editor(df, hide_index=True)
