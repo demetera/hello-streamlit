f4e66b31be17c264c17cac24d3be641f5ef96d67
Demetera <hal4ed@gmail.com>
2025-03-06 00:15:12 +0100

    update

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=["a", "b", "c"])
+chart_data = pd.DataFrame(np.random.randn(10, 2), columns=["a", "b"])
 
 st.bar_chart(chart_data)
