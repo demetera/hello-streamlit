e1575300f0a58fd71f394bd39e65650e527447dd
Demeter Abroncs <hal4ed@gmail.com>
2025-03-07 23:29:40 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,18 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
+import requests
+import json
 
-chart_data = pd.DataFrame(np.random.randn(10, 2), columns=["a", "b"])
+data = requests.get('https://maps.nextbike.net/maps/nextbike-live.json?city=699&domains=bh&list_cities=0&bikes=0')
+data_json = json.loads(data.text)
+data_city = data_json['countries'][0]['cities'][0]['places']
 
-st.bar_chart(chart_data)
+st.write(len(data_city))
+# st.write(data_city[0])
+
+bike_df = pd.DataFrame(columns=['Name', 'Bikes'])
+
+for item in data_city:
+    bike_df.loc[len(bike_df)] = [item['name'], item['bikes_available_to_rent']]
+
+st.dataframe(bike_df, hide_index=True)
