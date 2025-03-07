817fb167eeda75cb26e773a5f9760bcac96109d7
Demeter Abroncs <hal4ed@gmail.com>
2025-03-07 23:46:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,12 +7,17 @@ data = requests.get('https://maps.nextbike.net/maps/nextbike-live.json?city=699&
 data_json = json.loads(data.text)
 data_city = data_json['countries'][0]['cities'][0]['places']
 
-st.write(len(data_city))
+st.title('Budapest MOL bubi bikes app')
+
+st.write(f'Total stations = {len(data_city)}')
+st.write('Sorted by available bikes')
 # st.write(data_city[0])
 
 bike_df = pd.DataFrame(columns=['Name', 'Bikes'])
 
 for item in data_city:
     bike_df.loc[len(bike_df)] = [item['name'], item['bikes_available_to_rent']]
+
+bike_df.sort_values(by=['Bikes'], inplace=True, ascending=False)
 
 st.dataframe(bike_df, hide_index=True)
